bbc1eaa9aa1b556292256eeb886a5eacb9d12023
minjeadev <93039870+minjeadev@users.noreply.github.com>
2022-06-04 22:10:37 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,18 @@ const chats = {};
 let isInt = (n) => !isNaN(n) && Number.isInteger(Number(n));
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if (device.getBatteryLevel()>=15) {Api.makeNoti("전원이 부족해요!","배터리가 부족하여, 봇의 작동을 중단시켰어요.",444);Api.off();return;};
+   
+    let date = new Date();
+    
     let cmd = msg.trim().split(" ");
     let vb = Api.getContext().getSystemService(android.content.Context.VIBRATOR_SERVICE);
     
     if(!chats[sender]) chats[sender] = {last:null};
     if(chats[sender].last==msg) return;
     chats[sender].last = msg;
+    
+    if(cmd[0]=="!시간") return replier.reply("현재 시각은, "+date);
     
     // 여기부턴 !명령어 인수입니다.
     if(cmd.length<=1) return;
@@ -28,8 +34,10 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         vb.vibrate(Number(cmd[1])*1000);
     }
     
+    
     // 여기는 알아서 비활성화 해주세요.
     if (cmd[0] == "!ip") {
+        Api.showToast("누군가 나의 아이피를 조회함. 채팅방: "+room);
         return replier.reply(org.jsoup.Jsoup.connect("http://api.ipify.org").get().text());
     }
 }
